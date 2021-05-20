c63cd021e2c5220d2927eec743bc330f81fb80eb
surya <suryabalasundaram1207@gmail.com>
2021-05-20 08:15:07 +0530

    Committed

--- Ecommerce_Django/asgi.py
+++ Ecommerce_Django/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for grocery_shop project.
+ASGI config for Ecommerce_Django project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'grocery_shop.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Ecommerce_Django.settings')
 
 application = get_asgi_application()
